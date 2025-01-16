901a461c6dd27563954ab999c2a510b7f414c4dc
PoalNinh <ninhphuoc@phuocnv.io.vn>
2025-01-16 09:06:18 +0700

    123

---
+++ key.js
@@ -0,0 +1,12 @@
+   // Constants
+   const APP_ID = 'a3c21623-f7da-4dfd-bbe4-197bd67d7296';
+   const ACCESS_KEY = 'V2-5WcPS-sJgmv-8IXNy-QnfZ7-8sfxW-8XiP6-bZe4T-IaHdV';
+   const REGION = 'www';
+   const API_URL = `https://${REGION}.appsheet.com/api/v2/apps/${APP_ID}/tables`;
+   const IMGBB_API_KEY = '7267d539a8c541130ce4524afdc69ad3';
+
+   const API_CONFIG = {
+    APP_ID: APP_ID,
+    ACCESS_KEY: ACCESS_KEY,
+    BASE_URL: "https://www.appsheet.com/api/v2/apps"
+};
